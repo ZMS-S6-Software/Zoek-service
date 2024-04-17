2fbac01e75f19d1cdddccbfa5122a27ad635d5bd
Mattijs Pronk <456410@student.fontys.nl>
2024-04-17 09:43:43 +0200

    rabbitmq toegevoegd

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // iets importeren
-import * as fs from 'fs';
-import {fileReader} from 'fs';
 
 //nodejs documentatie
 // https://nodejs.org/api/fs.html
---
+++ receive.js
@@ -0,0 +1,16 @@
+import {connect} from 'amqplib';
+
+const connection = await connect('amqp://localhost');
+
+const channel = await connection.createChannel();
+
+const queue = 'message';
+
+await channel.assertQueue(queue, { durable:false });
+
+channel.consume(queue, (msg) => {
+    console.log(`Received ${msg.content.toString()}`);
+});
+
+// run in docker
+// docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq
