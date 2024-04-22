025f02b050a7dab1b8d16506ae2b1709eee97f03
Mattijs Pronk <mattijspronk99@gmail.com>
2024-04-22 13:26:04 +0200

    docker changes

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// iets importeren
+import {connect} from 'amqplib';
 
-//nodejs documentatie
-// https://nodejs.org/api/fs.html
+const connection = await connect('amqp://172.20.0.2');
+
+const channel = await connection.createChannel();
+
+const queue = 'message';
+
+await channel.assertQueue(queue, { durable:false });
+
+channel.consume(queue, (msg) => {
+    console.log(`Received ${msg.content.toString()}`);
+});
