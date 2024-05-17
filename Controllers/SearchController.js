87d99a877a0fd472e5152651f85f9385541927b2
Mattijs Pronk <456410@student.fontys.nl>
2024-05-17 10:23:55 +0200

    Update SearchController.js

--- Controllers/SearchController.js
+++ Controllers/SearchController.js
@@ -4,7 +4,7 @@ const patientService = patientLogic();
 
 
 // const connection = await connect(`amqp://${process.env.RABBITMQ_HOST || 'localhost'}`);
-const connection = await connect('amqp://rabbitmq:5672');
+const connection = await connect('amqp://rabbitmq');
 const channel = await connection.createChannel();
 const queue = 'message';
 await channel.assertQueue(queue, { durable:false });
